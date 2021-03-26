c294343b7dfbd002b8421e8529a43d97559b9183
Kishor <kishor1124@gmail.com>
2021-03-26 08:09:07 +0530

    components

--- hello-app/src/app/app.module.ts
+++ hello-app/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { PersonComponent } from './person/person.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, EmployeeComponent, PersonComponent
   ],
   imports: [
     BrowserModule
